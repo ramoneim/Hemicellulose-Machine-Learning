ca0f85905caecdae9e70ed6085bf174263b98e8f
ramoneim <35639718+ramoneim@users.noreply.github.com>
2021-08-19 09:43:09 -0700

    Update and rename Method1ScipyInt.py to Scipy 1-step.py
    Added comments and name change for clarity

--- Hybrid Models/Scipy 1-step.py
+++ Hybrid Models/Scipy 1-step.py
@@ -44,7 +44,7 @@ labels_short = [factors]
 X_all = pd.concat([X_nonscaled], ignore_index=True,axis=1)
 X_all.columns = finalCols 
 
-# Looking at distribution of Yield Data
+
 Y = (df['Yield'])
 y_train = Y.tolist()
 YIELD_train = y_train
@@ -67,6 +67,7 @@ Series_train = X_all['New Series?'].tolist()
 
 rows = [] 
 
+# Converting acid concentration and temperature
 for i in range(len(CA_train)):
     CA_train[i]=CA_train[i]*98.079/2/10
     if Temp_train[i]>450:
@@ -98,7 +99,7 @@ for i in range(len(Temp_set)):
 Yield_set = []
 TotalT_set = []
 
-    
+# Creating list of lists for yield and time for each experimental set        
 for i in range(len(rows)):
     index = rows[i]
     settime = []
@@ -134,20 +135,20 @@ for i in lastset:
 TotalT_set.append(settime)
 Yield_set.append(settime)
 
+# Creating a weights array that is proportional to the number of datapoints in each experiment
 for i in range(len(TotalT_set)):
     array = TotalT_set[i]
     length = len(array)
     Weights_set[i] = length
     
 YieldSol_train = np.zeros(len(CA_set))
-# YieldSol_test = np.zeros(len(X_test))
 
 
 feats = range(14)
 X_new = np.zeros((287,14))
 Y = np.zeros(287)
 
-
+# Defining X and Y for the model
 for i in range(len(Temp_set)):
     index=rows[i]
     X_new[i,0]=Temp_train[index]
@@ -167,15 +168,17 @@ for i in range(len(Temp_set)):
     #X_new[i,14]=YIELD_train[index]
     Y[i]=YIELD_train[index]
 
+# Converting all X values to float    
 X_newnew = []
 X_new = np.array(X_new, dtype=float) #  convert using numpy
 for i in range(len(X_new)):
     row = X_new[i,:]
     rownew = [float(i) for i in row] 
     X_newnew.append(rownew)
-    
+
+
+# Converting all Y values to float     
 Yield_set_new = []
-# Yield_set = np.array(Yield_set, dtype=float)
 for i in range(len(Yield_set)):
     rowyield = Yield_set[i]
     rowyield = [float(i) for i in rowyield]
@@ -186,16 +189,19 @@ Yield_set = Yield_set_new
 Y = np.array(Y, dtype=float) #  convert using numpy
 Y = [float(i) for i in Y] 
     
+# Splitting into training and testing sets    
 X_train, X_test, y_train, y_test = train_test_split(X_newnew, Y, test_size=0.2, random_state=None, shuffle=False)
 
 Yield_set = np.transpose(np.transpose(Yield_set))
 
+# Function for flattening arrays or list of lists
 def flatten(x):
     if isinstance(x, collections.Iterable):
         return [a for i in x for a in flatten(i)]
     else:
         return [x]
 
+# Defining all kinetic functions
 def getK (A, E, Ca, m, T):
     # A in min^(-1); E in kJ/mol, Ca in %, and T in K
     if T>450:
@@ -225,6 +231,7 @@ def getX(k1,k2, H0, t):
     denom = k1 - k2
     return num/denom
 
+# Defining ODEs for SOLVE IVP 
 def dxdt(t, y, i, index_params, params):
     # h, x = y
     A1, E1, m1, A2, E2, m2 = getkinparams(index_params, params)
@@ -244,18 +251,8 @@ def getkinparams(index_params, params):
     return (A1, E1, m1, A2, E2, m2)
 
 
-
+# Defining ODEs for SOLVE IVP - dif version 
 def dxdtnew(t, y, CA, T, A1, A2, m1, m2, E1, E2):
-    # A1 = params[index_params]
-    # E1 = params[index_params+1]
-    # m1 = params[index_params+2]
-    # A2 = params[index_params+3]
-    # E2 = params[index_params+4]
-    # m2 = params[index_params+5]
-    # T = X_train[i][0]
-    # CA = X_train[i][2]
-    # if T>450:
-    #     T=450
     if CA>0: 
         k1 = A1*CA**m1*math.exp(-E1/(8.3143e-3*T))
         k2 = A2*CA**m2*math.exp(-E2/(8.3143e-3*T))
@@ -267,69 +264,7 @@ def dxdtnew(t, y, CA, T, A1, A2, m1, m2, E1, E2):
     return [dhdt, dxdt]
 
 
-# def get_error(params):
-#     y_train_pred = []
-#     for i in range(len(X_train)):
-#         # T = X_train[i,0]
-#         T = X_train[i][0]
-#         # LSR = X_train[i,1]
-#         LSR = X_train[i][1]
-#         # CA = X_train[i,2]
-#         CA = X_train[i][2]
-#         # Fx = X_train[i,3]
-#         Fx = X_train[i][3]
-#         H0 = (Fx/100)/(LSR+1)*1000
-#         y_set = np.zeros_like(Yield_set[i])
-#         y_set = Yield_set[i]
-#         Yield_solset = np.zeros_like(y_set)
-#         Time_set = TotalT_set[i]
-#         print('Index: ', i)
-#         x1, x2, x3, x4, x5, y1, y2, y3, y4, y5, z1, z2, z3, z4, z5, w1, w2, w3, w4, w5, l1, l2 , l3, l4, l5, o1, o2, o3, o4, o5 = params[1374:]
-#         print('T, LSR, CA, Fx, H0, yset, timeset: ', T, LSR, CA, Fx, H0, y_set, Time_set)
-#         # if i!=0:
-#         index_params  = i*6
-#         for j in range(len(y_set)):
-#             A1 = params[index_params]
-#             E1 = params[index_params+1]
-#             m1 = params[index_params+2]
-#             A2 = params[index_params+3]
-#             E2 = params[index_params+4]
-#             m2 = params[index_params+5]
-#             # print('A1, E1, m1, A2, E2, m2: ', A1, E1, m1, A2, E2, m2)
-#             A1 = x1*T+x2*LSR+x3*CA+x4*Fx+x5
-#             E1 = y1*T+y2*LSR+y3*CA+y4*Fx+y5
-#             m1= z1*T+z2*LSR+z3*CA+z4*Fx+z5
-#             A2 = w1*T+w2*LSR+w3*CA+w4*Fx+w5
-#             E2 = l1*T+l2*LSR+l3*CA+l4*Fx+l5
-#             m2 = o1*T+o2*LSR+o3*CA+o4*Fx+o5
-#             t = Time_set[j]
-#             k1=getK(A=A1, E=E1, Ca=CA, m=m1, T=T)
-#             k2=getK(A=A2, E=E2, Ca=CA, m=m2, T=T)
-#             if (k1==k2):
-#                 k2=k1*10
-#             print('k1 value: ', k1)
-#             print('k2 value: ', k2)
-#             X_sol = getX(k1=k1, k2=k2, H0=H0, t=t)
-#             # if CA==0:
-#             #     logk1=np.log(A1)-(E1/(R*T))
-#             #     logk2=np.log(A2)-(E2/(R*T))
-#             # else:
-#             #     logk1=np.log(A1)*m1*np.log(CA)-(E1/(R*T))
-#             #     logk2=np.log(A2)*m2*np.log(CA)-(E2/(R*T))
-#             # X_sol = (-np.exp(logk1)*H0*(math.exp(-np.exp(logk1)*t)-math.exp(-np.exp(logk2)*t)))/(np.exp(logk1-logk2))
-#             Yield_solset[j] = 100*X_sol*LSR/(1000*(Fx/100))
-#         y_train_pred.append(Yield_solset)
-#         # print('y_train_pred ', y_train_pred)
-#     y_train_real = []
-#     for i in range(len(y_train_pred)):
-#         yrow = Yield_set[i]
-#         y_train_real.append(yrow)
-#     y_train_pred = flatten(y_train_pred)
-#     y_train_real = flatten(y_train_real)
-#     print('y pred, y real: ', y_train_pred, y_train_real)
-#     error = metrics.mean_absolute_error(y_train_real, y_train_pred)
-#     return error
-
+# Defining function that gives yield error 
 def get_error(params):
     y_train_pred = []
     for i in range(len(X_train)):
@@ -343,9 +278,8 @@ def get_error(params):
         Yield_solset = np.zeros_like(y_set)
         Time_set = TotalT_set[i]
         print('Index: ', i)
+        # Extracting all linear parameters
         x1, x2, x3, x4, x5, y1, y2, y3, y4, y5, z1, z2, z3, z4, z5, w1, w2, w3, w4, w5, l1, l2 , l3, l4, l5, o1, o2, o3, o4, o5 = params[1374:]
-        # print('T, LSR, CA, Fx, H0, yset, timeset: ', T, LSR, CA, Fx, H0, y_set, Time_set)
-        # if i!=0:
         index_params  = i*6
         for j in range(len(y_set)):
             A1 = params[index_params]
@@ -354,6 +288,7 @@ def get_error(params):
             A2 = params[index_params+3]
             E2 = params[index_params+4]
             m2 = params[index_params+5]
+            # Defining linear relationship between features and kinetic parameters
             A1 = x1*T+x2*LSR+x3*CA+x4*Fx+x5
             E1 = y1*T+y2*LSR+y3*CA+y4*Fx+y5
             m1 = z1*T+z2*LSR+z3*CA+z4*Fx+z5
@@ -361,16 +296,20 @@ def get_error(params):
             E2 = l1*T+l2*LSR+l3*CA+l4*Fx+l5
             m2 = o1*T+o2*LSR+o3*CA+o4*Fx+o5
             t = Time_set[j]
+        # Defining solve ivp parameters    
         t_0 = min(Time_set)-5
         t_max = max(Time_set)+5
         t_span = [t_0, t_max]   
         init_vals = [H0, 0]
         t_eval = Time_set
+        # Integrating the ODE to solve
         sol = solve_ivp(dxdt, t_span, init_vals, t_eval = t_eval, args=(i, index_params, params), dense_output=True)
+        # Calculating yield using calculated Xf concentration
         for j in range(len(y_set)):
             Yield_solset[j] = 100*sol.y[1][j]*LSR/(1000*(Fx/100))
         y_train_pred.append(Yield_solset)
         # print('y_train_pred: ', y_train_pred)
+    # Arranging and flattening y's and calculating MAE
     y_train_real = []
     for s in range(len(y_train_pred)):
         yrow = Yield_set[s]
@@ -386,17 +325,16 @@ def get_error(params):
 kinetic_params = []
 errorsab = [] 
 
-
+# Defining and organizing initial guess for minimization problem 
 initialGuessparams = [4.67e16, 142.58, 1.75, 6.51e16, 155.36,1]
 variablesguess = np.ones(30)
-#varbs = concatenate((initialGuessparams, variablesguess), axis=0)
-#print(varbs)
 initialguess = []
 for i in range(len(X_train)):
     initialguess.append(initialGuessparams)
 initialguess = np.transpose(np.transpose(initialguess))
 initialguess=initialguess.flatten()
 
+# Bounds for features if using minimize instead of fmin -- both okay
 lowerbound = [1e8, 50, 0.1, 1e8, 50, 0.1]
 upperbound = [1e30, 300, 1, 130, 300, 3]
 lbvs = -((np.ones(30))*np.inf)
@@ -423,11 +361,11 @@ lb = concatenate((lb,lbvs), axis=0)
 
 bounds = concatenate((lb,ub), axis=-1)
 
-
+# Defining scipy optimization function
 initialguess = concatenate((initialguess, variablesguess), axis=0)
 initialguess = np.array(initialguess, dtype=float) #  convert using numpy
 intialguess = [float(i) for i in initialguess] 
-#output = optimize.minimize(get_error, initialguess)
+#output = optimize.minimize(get_error, initialguess, bounds=bounds)
 output = optimize.fmin(get_error, initialguess,maxiter=10000, full_output=1)
 minimum = output[0]
 errors = output[1]
